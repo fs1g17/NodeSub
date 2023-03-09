46df0e319175d0b5ed2f54364ce711a47e2afbb8
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2023-03-09 18:55:02 +0000

    Add authentication endpoint

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ require('body-parser-xml')(bodyParser);
 const app = express();
 app.use(bodyParser.xml());
 
-app.post('/poop', (req, res) => {
+app.get('/', (req, res) => {
+  const challenge = req.query['hub.challenge'];
+
+  if(typeof challenge === 'undefined') {
+    res.status(400).send('No challenge detected');
+    return;
+  }
+
+  console.log('Authenticated');
+  res.send(challenge.toString());
+})
+
+app.post('/', (req, res) => {
   // parse request, get xml feed, update db  
   const videoLink = req.body.feed.entry[0].link[0]['$'].href;
 
